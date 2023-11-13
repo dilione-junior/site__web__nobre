1a7b83afcfd0e63933f43160f33637369efb69d9
dilione <143102631+dilione-junior@users.noreply.github.com>
2023-11-13 16:44:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const acionamento = document.querySelector("#link__mais__info");
+acionamento.addEventListener("click", mostraValor);
+
+function mostraValor(){
+    alert("Obrigado por continuar ! 😸 🎈");
+}
