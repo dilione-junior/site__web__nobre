e7d56e73bab046c6ad141b16553b631d5d561380
dilione <143102631+dilione-junior@users.noreply.github.com>
2023-12-03 18:56:30 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,6 @@
+const enviar = document.querySelector("#botao__formulario");
+enviar.addEventListener("click", mostraValor);
+
+function mostraValor(){
+    alert("Obrigado por entrar em Contato");
+}
